1a832179fc53538583dd1cebea14a08baddbbdc7
Fox <urboifox@gmail.com>
2025-03-09 01:02:30 +0200

    restore dist in git ignore

--- dist/products/dto/update-product.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { Prisma } from '@prisma/client';
-export declare class UpdateProductDto implements Prisma.ProductCreateInput {
-    name: string;
-    price: number;
-}
--- dist/users/entities/user.entity.d.ts
+++
@@ -1,8 +0,0 @@
-import { User as PrismaUser } from '@prisma/client';
-export declare class User implements PrismaUser {
-    id: number;
-    name: string;
-    email: string;
-    password: string;
-    createdAt: Date;
-}
