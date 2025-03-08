9301e33b1b8ac3cd09ed6bf6591085881b75ced1
Fox <urboifox@gmail.com>
2025-03-09 01:26:35 +0200

    add swagger to vercel serverless function

--- src/vercel.ts
+++ src/vercel.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors(); // Enable CORS if needed
     await app.init();
+
+    const config = new DocumentBuilder().setTitle('Nowaste API').setVersion('1.0').build();
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api/docs', app, documentFactory);
 
     // Get the HTTP adapter
     const expressApp = app.getHttpAdapter().getInstance();
