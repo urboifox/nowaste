3dc15e20d223ce077200f19b628a1a4d754369fc
Fox <urboifox@gmail.com>
2025-03-09 02:02:45 +0200

    add custom swagger css

--- src/vercel.ts
+++ src/vercel.ts
@@ -7,8 +7,22 @@ async function bootstrap() {
     app.enableCors(); // Enable CORS if needed
 
     const config = new DocumentBuilder().setTitle('Nowaste API').setVersion('1.0').build();
-    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    const documentFactory = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api/docs', app, documentFactory);
+
+    SwaggerModule.setup('swagger', app, documentFactory, {
+        customSiteTitle: 'Backend Generator',
+        customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
+        customJs: [
+            'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+        ],
+        customCssUrl: [
+            'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+            'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+            'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+        ],
+    });
 
     await app.init();
 
